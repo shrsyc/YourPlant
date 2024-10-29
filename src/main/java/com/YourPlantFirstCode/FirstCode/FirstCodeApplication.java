18f46b731629493dc941de44c32ab371d0bff221
shrsyc <shrsyc@gmail.com>
2024-10-29 22:06:45 +0530

    first commit

---
+++ src/main/java/com/YourPlantFirstCode/FirstCode/FirstCodeApplication.java
@@ -0,0 +1,13 @@
+package com.YourPlantFirstCode.FirstCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/YourPlantFirstCode/FirstCode/FirstCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YourPlantFirstCode.FirstCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
